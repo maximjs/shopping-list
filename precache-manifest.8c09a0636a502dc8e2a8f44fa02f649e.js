0fe8cf81d0c5df07f7c48d4201be6e7c11ce46ba
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2019-03-27 21:46:33 +0300

    Updates

---
+++ precache-manifest.8c09a0636a502dc8e2a8f44fa02f649e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b7aae557ca63abebda0",
+    "url": "/shopping-list/static/css/main.9272ad9b.chunk.css"
+  },
+  {
+    "revision": "1b7aae557ca63abebda0",
+    "url": "/shopping-list/static/js/main.364987f8.chunk.js"
+  },
+  {
+    "revision": "f83e989e4d16bb609454",
+    "url": "/shopping-list/static/js/runtime~main.e244f075.js"
+  },
+  {
+    "revision": "8fd4f464bbb7df5075dd",
+    "url": "/shopping-list/static/css/2.6e93ed76.chunk.css"
+  },
+  {
+    "revision": "8fd4f464bbb7df5075dd",
+    "url": "/shopping-list/static/js/2.ab770bbb.chunk.js"
+  },
+  {
+    "revision": "7c266afd7856ea731fdba99838bec455",
+    "url": "/shopping-list/index.html"
+  }
+];
